a2f421404c47b654aa7fa60c290291b9e82462db
phillip <phillip@wadecki.de>
2023-12-17 09:34:21 +0100

    add day 15

--- readme.md
+++ readme.md
@@ -25,7 +25,7 @@
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%86/yellow)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -167,9 +167,9 @@ Both parts: -
 
 ```
 Day 15
-Time part 1: -
+Time part 1: 2.115ms
 Time part 2: -
-Both parts: -
+Both parts: 2.115ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 61.906ms
+Total stars: 9/50
+Total time: 64.02ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day15/index.js
@@ -0,0 +1,57 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  var result = input.split(',')
+    .reduce((result, c) => 
+      result + c.split('')
+                .reduce((curr, val) => 
+                  ((curr + val.charCodeAt()) * 17) % 256, 0)
+      , 0)
+  // charCodeAt
+  return result.toString();
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: 'HASH',
+        expected: "52"
+      },
+      {
+        input: `rn=1`,
+        expected: "30",
+      },
+      {
+        input: `rn=1,cm-`,
+        expected: "283",
+      },
+      {
+        input: `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`,
+        expected: "1320",
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
